d720c8c7397782628b48a5eb4a6b15be10731a9a
Andy <andychen.105@gmail.com>
2026-03-08 21:45:17 -0400

    Changed binID to binRoute on the router page so BinViewPage can destructure the right param Co-authored-by: Andy Chen <81591593+andychen3@users.noreply.github.com> Co-authored-by: Mordecai Coleman <105605981+mordcole@users.noreply.github.com>

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -14,7 +14,7 @@ export const router = createBrowserRouter([
       },
       {
         element: <BinViewPage />,
-        path: "bins/:binId",
+        path: "/bins/:binRoute",
       },
     ],
   },
